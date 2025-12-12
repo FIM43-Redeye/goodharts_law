8284cd3b7985e53d8c40246ae1df148aadb647a3
FIM43-Redeye <14264359+FIM43-Redeye@users.noreply.github.com>
2025-12-12 14:11:21 -0500

    Finish refactor: cleanup simulation.py duplicates and remove legacy behaviors.py

--- simulation.py
+++ simulation.py
@@ -9,19 +9,11 @@ class Simulation:
         self.world = World(config['GRID_WIDTH'], config['GRID_HEIGHT'], config)
         self.world.place_food(config['GRID_FOOD_INIT'])
         self.world.place_poison(config['GRID_POISON_INIT'])
-    def __init__(self, config):
-        self.config = config
-        self.world = World(config['GRID_WIDTH'], config['GRID_HEIGHT'], config)
-        self.world.place_food(config['GRID_FOOD_INIT'])
-        self.world.place_poison(config['GRID_POISON_INIT'])
 
         self.agents = []
         for _ in range(config['GRID_AGENTS_INIT']):
-        for _ in range(config['GRID_AGENTS_INIT']):
             randx = np.random.randint(0, self.world.width)
             randy = np.random.randint(0, self.world.height)
-            behavior = GreedyFoodSeeker()
-            self.agents.append(Organism(randx, randy, config['ENERGY_START'], config['AGENT_VIEW_RANGE'], self.world, behavior, config))
             behavior = GreedyFoodSeeker()
             self.agents.append(Organism(randx, randy, config['ENERGY_START'], config['AGENT_VIEW_RANGE'], self.world, behavior, config))
 
@@ -31,13 +23,11 @@ class Simulation:
                 self.agents.remove(agent)
                 continue
             agent.update()
-            agent.update()
 
     def get_render_grid(self):
         render_grid = self.world.grid.copy()
         for agent in self.agents:
             if 0 <= agent.x < self.world.width and 0 <= agent.y < self.world.height:
-                render_grid[agent.y, agent.x] = 4
                 render_grid[agent.y, agent.x] = 4
         return render_grid
 
